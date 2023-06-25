d28852466dd72d9cc1ab13733a16a6c22280354d
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-06-25 17:43:15 -0600

    folders and files, server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes/api");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
