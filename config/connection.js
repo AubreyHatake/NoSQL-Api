d3858ab77b9947e9b3573d7b72ecb5588f4dc40d
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-06-26 15:15:19 -0600

    connection.js

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
