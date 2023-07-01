b23a78ecd1c698daf172a35358cd4425110b933c
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-07-01 16:46:28 -0600

    utils, routes, npm i

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+// /api/thoughts
+router.route("/").get(getAllThought).post(createThought);
+
+// /api/thoughts/:id
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/user-controller");
+
+// /api/users
+router.route("/").get(getAllUser).post(createUser);
+
+// /api/users/:id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
