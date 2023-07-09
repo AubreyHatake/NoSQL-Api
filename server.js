d781fea2718cec90f3d59b001a4cee1fba22e831
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-07-08 23:07:50 -0600

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const db = require("./config/connection");
-const routes = require("./routes/api");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
